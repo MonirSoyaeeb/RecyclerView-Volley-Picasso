7adedb35fb8b2f68c373754c5da606eafac259ca
Soyaeeb <monirul15-1742@diu.edu.bd>
2021-02-24 17:53:41 +0600

    Full (RecyclerView+Picasso+Volley Library combine Project)

---
+++ app/src/main/java/com/monir/volleypicassorecyclerview/ExampleAdapter.java
@@ -0,0 +1,85 @@
+package com.monir.volleypicassorecyclerview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.String.valueOf;
+
+public class ExampleAdapter extends RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder> {
+    private Context mContext;
+    private ArrayList<ExampleItem> mExampleList;
+    private OnItemClickLister lister;
+
+    public void SetOnItemCLickLister(OnItemClickLister lister) {
+        this.lister = lister;
+    }
+
+    public interface OnItemClickLister {
+        void OnItemClick(int position);
+    }
+
+
+    public ExampleAdapter(Context context, ArrayList<ExampleItem> exampleList) {
+        mContext = context;
+        mExampleList = exampleList;
+    }
+
+    @Override
+    public ExampleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.example_row, parent, false);
+        return new ExampleViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ExampleViewHolder holder, int position) {
+        ExampleItem currentItem = mExampleList.get(position);
+        String imageUrl = currentItem.getImageUrl();
+        String creatorName = currentItem.getCreator();
+        int likeCount = currentItem.getLikeCount();
+        holder.mTextViewCreator.setText(creatorName);
+        holder.mTextViewLikes.setText("Likes: " + likeCount);
+        Picasso.with(mContext).load(imageUrl).fit().centerInside().into(holder.mImageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mExampleList.size();
+    }
+
+    public class ExampleViewHolder extends RecyclerView.ViewHolder {
+        public ImageView mImageView;
+        public TextView mTextViewCreator;
+        public TextView mTextViewLikes;
+
+        public ExampleViewHolder(View itemView) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.image_view);
+            mTextViewCreator = itemView.findViewById(R.id.text_view_creator);
+            mTextViewLikes = itemView.findViewById(R.id.text_view_likes);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (lister != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            lister.OnItemClick(position);
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
